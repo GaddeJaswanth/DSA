d9c139e69438875144fa2d125f69e023e42d0973
Gadde Jaswanth <82304312+GaddeJaswanth@users.noreply.github.com>
2023-08-01 04:49:35 +0530

    Search Insert Position

---
+++ Arrays/SearchInsertPosition.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public static int searchInsert(int [] arr, int m){
+        // Write your code here.
+        if(arr[0]>=m){
+            return 0;
+        }
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]==m){
+                return i;
+            }
+            if(arr[i-1]<m && arr[i]>m){
+                return i;
+            }
+        }
+        if(arr[arr.length-1]<=m){
+            return arr.length;
+        }
+        return -1;
+    }
+}
+
+//Find appropriate position for 'm' in 'arr'
